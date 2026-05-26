3ffeeeada0726806082b1c4d19a6e72b862bb5ed
Tabrez Ahmed <tabrezhere@gmail.com>
2026-05-26 16:16:33 +0530

    version2

---
+++ Domain/Entities/LeaveRequest.cs
@@ -0,0 +1,9 @@
+public class LeaveRequest
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Reason { get; set; }
+    public bool IsApproved { get; set; }
+}
