f6df0e08284ba534131863daf5a8de4f880f5978
Tabrez Ahmed <tabrezhere@gmail.com>
2026-05-26 16:16:37 +0530

    version2

---
+++ Domain/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,8 @@
+public interface IDepartmentRepository
+{
+    Task<Department> GetByIdAsync(int id);
+    Task<IEnumerable<Department>> GetAllAsync();
+    Task AddAsync(Department department);
+    Task UpdateAsync(Department department);
+    Task DeleteAsync(int id);
+}
