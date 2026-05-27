20b59997ebd019b0cb03bccc8986473f07d4dc7e
Tabrez Ahmed <tabrezhere@gmail.com>
2026-05-27 16:40:53 +0530

    HTC AI EMp Spec

--- EmployeeManagementSystem/GlobalUsings.cs
+++ EmployeeManagementSystem/GlobalUsings.cs
@@ -1,7 +1,6 @@
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using System.Net;
 global using System.Net.Http;
 global using System.Threading.Tasks;
 global using FluentValidation;
